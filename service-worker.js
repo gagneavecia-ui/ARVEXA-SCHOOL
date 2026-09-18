ac36d95c02bf7e52e8f1a3b25106d825dc7fbc54
gagneavecia-ui <gagneavecia@gmail.com>
2026-09-18 13:15:20 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // Ne touche PAS aux CDN externes (Firebase, Google Fonts, FontAwesome)
 // ================================================================
 
-const CACHE_VERSION = 'arvexa-v1.0.12';
+const CACHE_VERSION = 'arvexa-v1.0.13';
 const CACHE_STATIC = `${CACHE_VERSION}-static`;
 const CACHE_DYNAMIC = `${CACHE_VERSION}-dynamic`;
 
